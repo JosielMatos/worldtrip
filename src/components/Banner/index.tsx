d0d69e4d05dac791040b4c7f80758533e80b2350
JosielMatos <josielmatos77@protonmail.com>
2022-05-18 15:56:53 -0300

    Add banner background and heading

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,34 @@
+import { Flex, Heading, Text } from "@chakra-ui/react";
+
+export function Banner() {
+  return (
+    <Flex
+      w='100%'
+      h={["10.1875rem", "15.625rem", "15.625rem", "20.9375rem"]}
+      bgImage="url('/banner-bg.jpg')"
+      bgRepeat='no-repeat'
+      bgSize='cover'
+      bgPosition={["100% 20%", "100% 20%", "100% 30%"]}
+    >
+      <Flex
+        justify={["center", "space-between"]}
+        align='center'
+        w='100%'
+        px={["4", "10", "25", "20", "36"]}
+      >
+        <div>
+          <Heading
+            color='gray.100'
+            fontWeight='500'
+            fontSize={["xl", "2xl", "2xl", "2xl", "4xl"]}
+          >
+            5 Continentes, <br /> infinitas possibilidades.
+          </Heading>
+          <Text color="gray.300" mt="5" fontSize={["0.8rem", "xl"]} maxW={["100%", "100%", "100%", "34.375rem"]}>
+            Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+          </Text>
+        </div>
+      </Flex>
+    </Flex>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Flex } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import { Banner } from "../components/Banner";
 import { Header } from "../components/Header";
 
 const Home: NextPage = () => {
   return (
     <Flex direction={"column"}>
       <Header />
+      <Banner />
     </Flex>
   );
 };
