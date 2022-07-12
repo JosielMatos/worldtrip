51c513c3e2b51627fd5f354ea021f2f4eeef1eb6
JosielMatos <josielmatos77@protonmail.com>
2022-07-12 12:01:25 -0300

    Working build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "../styles/theme";
 
-function App({ Component, pageProps }: AppProps) {
+import { theme } from "../styles/theme";
+import "../styles/slide.scss";
+
+// Import Swiper styles
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
@@ -11,4 +18,4 @@ function App({ Component, pageProps }: AppProps) {
   );
 }
 
-export default App;
+export default MyApp;
